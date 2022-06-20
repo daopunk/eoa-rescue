6f771690f49a88a0069e571a28d14e3f1b3134d3
Hunter <50965054+hking2@users.noreply.github.com>
2022-06-20 13:34:24 -0600

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require('@nomiclabs/hardhat-etherscan');
+require('@openzeppelin/hardhat-upgrades');
+require('dotenv').config();
+require('@nomiclabs/hardhat-ethers');
+require('@nomiclabs/hardhat-waffle');
+require('hardhat-gas-reporter');
+
+module.exports = {
+  solidity: '0.8.4',
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.EXPOSED_PK],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      goerli: process.env.ETHERSCAN_KEY,
+      rinkeby: process.env.ETHERSCAN_KEY,
+      ropsten: process.env.ETHERSCAN_KEY,
+    },
+  },
+};
